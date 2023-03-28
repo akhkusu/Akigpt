e49ad3ce8a28d586b69be22559f027c9974901b1
akkusu <akkusu@ttu.ee>
2023-03-28 12:52:38 +0300

    akigpt.py

---
+++ akigpt.py
@@ -0,0 +1,30 @@
+import discord
+import openai
+import os
+
+
+client = discord.Client()
+openai.api_key = os.environ.get("OPENAI_API_KEY")
+
+@client.event
+async def on_ready():
+    print("Bot is ready.")
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith("!chat"):
+        prompt = message.content[6:]
+        response = openai.Completion.create(
+            engine="text-davinci-002",
+            prompt=prompt,
+            max_tokens=100,
+            n=1,
+            stop=None,
+            temperature=0.5,
+        )
+        await message.channel.send(response.choices[0].text)
+
+client.run("DISCORD_BOT_TOKEN")
